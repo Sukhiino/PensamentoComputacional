f0f1e14e97a55875892ac5256f7f31451a65c639
Sukhiino <136768723+Sukhiino@users.noreply.github.com>
2024-03-01 19:58:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.alert("Bem vindo a Pokédex")
+window.confirm('Aqui você irá encontrar a história de um pokémon')
+window.prompt("Vamos descobrir, quem é esse pokémon?")
+
+var nome = window.prompt("Qual é o seu nome treinador(a)?")
+window.alert("É um prazer te conhecer, " + nome + "!")
+
+var Pumpkaboo 
+var nome1 = Number.parseInt(window.prompt("Digite qual Pokémon você acha que é: "))
+var numero2 = Number.parseInt(window.prompt("Digite outro: "))
+var numero3 = Number.parseInt(window.prompt("Você tem mais uma chance: "))
+var Pumpkaboo = Number.Pumpkaboo(window.alert("O Pokémon é: "))
